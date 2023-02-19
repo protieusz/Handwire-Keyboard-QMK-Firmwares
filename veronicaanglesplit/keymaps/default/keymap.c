ac37bb64a48a516cb1919c876eac160fd47392d9
somepin <79652090+somepin@users.noreply.github.com>
2023-02-18 21:14:59 -0500

    remove enum
    not really needed for just one combo

--- veronicaanglesplit/keymaps/default/keymap.c
+++ veronicaanglesplit/keymaps/default/keymap.c
@@ -16,10 +16,6 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 };
 #endif
 
-enum combo_events {
-  COMBO_DEL
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q,        KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,
